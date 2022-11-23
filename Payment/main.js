0e570195bf008847de243ed27f949d8b921bbacb
A <A@suncake.com>
2022-11-23 18:31:53 +0700

    Update

--- Payment/main.js
+++ Payment/main.js
@@ -1,11 +1,29 @@
-
+var indexInit = 0
 document.querySelector("#sidebar-ct").querySelectorAll("li").forEach((item, index) => {
     item.addEventListener("click", (e) => {
-        const page = document.querySelectorAll(".content-page>div")
+        console.log(indexInit);
+        document.querySelector("#sidebar-ct").querySelectorAll("li")[indexInit].classList.remove("active")
+        item.classList.add("active");
         console.log(index);
-        for (let i = 0; i <= 3; i++) {
-            page[i].classList.remove("hide")
-        }
-        document.querySelectorAll(".content-page>div")[index].classList.add("hide")
+        indexInit = index
+        Page(index)
+
     })
 })
+const Page = (index) => {
+    const page = document.querySelectorAll(".content-page>div")
+    for (let i = 0; i <= 3; i++) {
+        page[i].classList.remove("hide")
+    }
+    document.querySelectorAll(".content-page>div")[index].classList.add("hide")
+
+}
+document.querySelector(".btn-primary").addEventListener("click", (e) => {
+    e.preventDefault()
+    document.querySelector(".popup").classList.add("show-popup")
+})
+document.querySelector(".popup").addEventListener("click", (e) => {
+    e.stopPropagation()
+    document.querySelector(".popup").classList.remove("show-popup")
+})
+Page(0)
